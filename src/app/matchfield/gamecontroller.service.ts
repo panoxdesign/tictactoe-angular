e5a90029b8dae5183b955a246f3ed5c1eb86e595
panoxdesign <panox.design@gmail.com>
2019-01-11 17:22:20 +0100

    update webpack dev server to version 3.1.14 for security reasons. minor bug fixes.

--- src/app/matchfield/gamecontroller.service.ts
+++ src/app/matchfield/gamecontroller.service.ts
@@ -6,7 +6,8 @@ import { Observable } from "rxjs";
   providedIn: "root"
 })
 export class GamecontrollerService {
-  private matchfield: number[] = new Array(9);
+  private readonly MATCHFIELD_LENGTH = 9;
+  private _matchfield: number[] = new Array(this.MATCHFIELD_LENGTH);
 
   private _status: GameStatus;
   private _actualPlayer: Player;
@@ -27,7 +28,7 @@ export class GamecontrollerService {
   constructor() {}
 
   reset() {
-    this.matchfield.fill(0, 0, 9);
+    this._matchfield.fill(0, 0, this.MATCHFIELD_LENGTH);
     this._status = GameStatus.RUNNING;
     this._actualPlayer = Player.X_PLAYER;
     this.emitEvent();
@@ -37,39 +38,37 @@ export class GamecontrollerService {
     if (this.isWon() || this.getField(fieldNumber)) {
       return;
     }
-    this.matchfield[fieldNumber] = this._actualPlayer;
+    this._matchfield[fieldNumber] = this._actualPlayer;
     this.calcWinning();
     if (!this.isWon()) {
       this._actualPlayer =
         this._actualPlayer === Player.X_PLAYER
           ? Player.O_PLAYER
           : Player.X_PLAYER;
-      this.emitEvent();
     }
+    this.emitEvent();
   }
 
   getField(fieldNumber: number): number {
-    return this.matchfield[fieldNumber];
+    return this._matchfield[fieldNumber];
   }
 
   private calcWinning() {
     for (let i = 0; i < this.winningSituations.length; i++) {
       if (
-        this.matchfield[this.winningSituations[i][0]] === this.actualPlayer &&
-        this.matchfield[this.winningSituations[i][1]] === this.actualPlayer &&
-        this.matchfield[this.winningSituations[i][2]] === this.actualPlayer
+        this._matchfield[this.winningSituations[i][0]] === this.actualPlayer &&
+        this._matchfield[this.winningSituations[i][1]] === this.actualPlayer &&
+        this._matchfield[this.winningSituations[i][2]] === this.actualPlayer
       ) {
         this._status = GameStatus.WINNER;
-        this.emitEvent();
       }
     }
 
     if (
       !this.isWon() &&
-      this.matchfield.find(value => value === 0) === undefined
+      this._matchfield.find(value => value === 0) === undefined
     ) {
       this._status = GameStatus.REMI;
-      this.emitEvent();
     }
   }
 
@@ -90,7 +89,7 @@ export class GamecontrollerService {
   private emitEvent() {
     this.gameEventEmitter.emit({
       status: this._status,
-      gameSnapshot: this.matchfield.slice(),
+      gameSnapshot: this._matchfield.slice(),
       actualPlayer: this.actualPlayer
     });
   }
