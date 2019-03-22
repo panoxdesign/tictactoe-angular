369f2b4e62cc633a8eeef829ed960c246a3ecb1c
Patrick <panox.design@gmail.com>
2019-03-22 15:48:34 +0100

    Version upgrades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FieldComponent } from "./matchfield/field/field.component";
 import { PlayerPipePipe } from "./matchfield/field/player-pipe.pipe";
 import { ResultComponent } from "./result/result.component";
 import { GamecontrolsComponent } from "./matchfield/gamecontrols/gamecontrols.component";
-import { GameeventPipePipe } from './result/gameevent-pipe.pipe';
+import { GameeventPipePipe } from "./result/gameevent-pipe.pipe";
 
 @NgModule({
   declarations: [
